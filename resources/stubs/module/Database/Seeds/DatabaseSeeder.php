2ed4e005da4e8904248d67dd6de89b21061afcec
Habib Nurrahman <nurmanhabib@yahoo.co.id>
2016-10-01 20:44:56 +0700

    Add default root seeder 'DatabaseSeeder' with namespace

---
+++ resources/stubs/module/Database/Seeds/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace DummyNamespace\Database\Seeds;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
