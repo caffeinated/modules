ee74d28ad1b014919c076465de93159bbafade91
nickurt <nickurt@users.noreply.github.com>
2018-06-25 10:00:27 +0200

    optimized and improved helpers-tests ...

--- src/Helpers/module.php
+++ src/Helpers/module.php
@@ -3,15 +3,15 @@
 use Caffeinated\Modules\Exceptions\ModuleNotFoundException;
 
 if (!function_exists('module_path')) {
-	/**
-	 * Return the path to the given module file.
-	 *
-	 * @param string $slug
-	 * @param string $file
-	 *
-	 * @return string
-	 * @throws \Caffeinated\Modules\Exceptions\ModuleNotFoundException
-	 */
+    /**
+     * Return the path to the given module file.
+     *
+     * @param string $slug
+     * @param string $file
+     *
+     * @return string
+     * @throws \Caffeinated\Modules\Exceptions\ModuleNotFoundException
+     */
     function module_path($slug = null, $file = '')
     {
         $modulesPath = config('modules.path');
@@ -25,45 +25,45 @@ if (!function_exists('module_path')) {
             );
         }
 
-        $filePath = $file ? '/'.ltrim($file, '/') : '';
+        $filePath = $file ? '/' . ltrim($file, '/') : '';
 
         if (is_null($slug)) {
             if (empty($file)) {
                 return $modulesPath;
             }
 
-            return $modulesPath.$filePath;
+            return $modulesPath . $filePath;
         }
 
         $module = Module::where('slug', $slug);
 
-	    if ( is_null($module) ) {
-		    throw new ModuleNotFoundException($slug);
-	    }
+        if (is_null($module)) {
+            throw new ModuleNotFoundException($slug);
+        }
 
-        return $modulesPath.'/'.$module['basename'].$filePath;
+        return $modulesPath . '/' . $module['basename'] . $filePath;
     }
 }
 
 if (!function_exists('module_class')) {
-	/**
-	 * Return the full path to the given module class.
-	 *
-	 * @param string $slug
-	 * @param string $class
-	 *
-	 * @return string
-	 * @throws \Caffeinated\Modules\Exceptions\ModuleNotFoundException
-	 */
+    /**
+     * Return the full path to the given module class.
+     *
+     * @param string $slug
+     * @param string $class
+     *
+     * @return string
+     * @throws \Caffeinated\Modules\Exceptions\ModuleNotFoundException
+     */
     function module_class($slug, $class)
     {
         $module = Module::where('slug', $slug);
 
-	    if ( is_null($module) ) {
-		    throw new ModuleNotFoundException($slug);
-	    }
+        if (is_null($module)) {
+            throw new ModuleNotFoundException($slug);
+        }
 
-        $namespace = config('modules.namespace').$module['basename'];
+        $namespace = config('modules.namespace') . $module['basename'];
 
         return "{$namespace}\\{$class}";
     }
