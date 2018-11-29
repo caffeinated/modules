cb36e7ba4f7b036e49899c29f7159f352556db5b
Shea Lewis (Kai) <shea.lewis89@gmail.com>
2018-11-29 15:18:34 -0800

    Add make:module test for custom mappings

---
+++ tests/Console/Generators/CommandMakeModuleTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Caffeinated\Modules\Tests\Commands\Generators;
+
+use Spatie\Snapshots\MatchesSnapshots;
+use Caffeinated\Modules\Tests\BaseTestCase;
+
+class CommandMakeModuleTest extends BaseTestCase
+{
+    use MatchesSnapshots;
+
+    protected $finder;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->finder = $this->app['files'];
+    }
+
+    /** @test */
+    public function it_can_generate_module_with_custom_mapping()
+    {
+        $this->app['config']->set("modules.locations.{$this->default}.mapping", [
+            'config'              => 'config',
+            'Database/Factories'  => 'src/Database/Factories',
+            'Database/Migrations' => 'src/Database/Migrations',
+            'Database/Seeds'      => 'src/Database/Seeds',
+            'Http/Controllers'    => 'src/Http/Controllers',
+            'Http/Middleware'     => 'src/Http/Middleware',
+            'Providers'           => 'src/Providers',
+            'Resources/Lang'      => 'resources/lang',
+            'Resources/Views'     => 'resources/views',
+            'Routes'              => 'routes'
+        ]);
+
+        $this->artisan('make:module', ['slug' => 'custom', '--quick' => 'quick']);
+
+        $this->assertDirectoryExists(module_path('custom').'/config');
+        $this->assertDirectoryExists(module_path('custom').'/src/Database/Factories');
+        $this->assertDirectoryExists(module_path('custom').'/src/Database/Migrations');
+        $this->assertDirectoryExists(module_path('custom').'/src/Database/Seeds');
+        $this->assertDirectoryExists(module_path('custom').'/src/Http/Controllers');
+        $this->assertDirectoryExists(module_path('custom').'/src/Http/Middleware');
+        $this->assertDirectoryExists(module_path('custom').'/src/Providers');
+        $this->assertDirectoryExists(module_path('custom').'/resources/lang');
+        $this->assertDirectoryExists(module_path('custom').'/resources/views');
+        $this->assertDirectoryExists(module_path('custom').'/routes');
+    }
+
+    public function tearDown()
+    {
+        $this->finder->deleteDirectory(module_path('custom'));
+
+        parent::tearDown();
+    }
+}
