464e5eda189e6fa3d4b5cd8bfefbdadcf49f81b2
nickurt <nickurt@users.noreply.github.com>
2020-02-24 21:08:31 +0100

    fix tests

--- tests/Console/Generators/__snapshots__/CommandMakeJobTest__it_can_generate_a_new_job_with_custom_module_namespace__1.php
+++ tests/Console/Generators/__snapshots__/CommandMakeJobTest__it_can_generate_a_new_job_with_custom_module_namespace__1.php
@@ -3,10 +3,10 @@
 namespace App\\CustomJobsNamespace\\Jobs\\Jobs;
 
 use Illuminate\\Bus\\Queueable;
-use Illuminate\\Queue\\SerializesModels;
-use Illuminate\\Queue\\InteractsWithQueue;
 use Illuminate\\Contracts\\Queue\\ShouldQueue;
 use Illuminate\\Foundation\\Bus\\Dispatchable;
+use Illuminate\\Queue\\InteractsWithQueue;
+use Illuminate\\Queue\\SerializesModels;
 
 class CustomJob implements ShouldQueue
 {
