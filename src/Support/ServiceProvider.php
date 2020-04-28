95fc0f23d3f09183418f9d68edcbfc82ac712473
Shane Krolikowski <skrolikowski@gmail.com>
2020-04-28 19:26:52 +0000

    new method to auto-magically load routes for module

--- src/Support/ServiceProvider.php
+++ src/Support/ServiceProvider.php
@@ -70,6 +70,50 @@ class ServiceProvider extends IlluminateServiceProvider
     }
 
     /**
+     * Load the given module routes files if not already cached.
+     *
+     * @param  string  $string
+     * @return void
+     */
+    protected function loadRoutesFor($slug)
+    {
+        if (! ($this->app instanceof CachesRoutes && $this->app->routesAreCached())) {
+            if (Module::exists($slug) && Module::isEnabled($slug)) {
+                $module = Module::where('slug', $slug);
+
+                if (file_exists(module_path($slug, 'routes/datatable.php'))) {
+                    Route::group([
+                        'middleware' => 'api',
+                        'namespace'  => "Modules\\{$module->get('basename')}\\Http\\Controllers\\DataTable",
+                        'prefix'     => 'datatable',
+                    ], function ($router) use ($slug) {
+                        require module_path($slug, 'routes/datatable.php');
+                    });
+                }
+
+                if (file_exists(module_path($slug, 'routes/web.php'))) {
+                    Route::group([
+                        'middleware' => 'web',
+                        'namespace'  => "Modules\\{$module->get('basename')}\\Http\\Controllers\\Web",
+                    ], function ($router) use ($slug) {
+                        require module_path($slug, 'routes/web.php');
+                    });
+                }
+
+                if (file_exists(module_path($slug, 'routes/api.php'))) {
+                    Route::group([
+                        'middleware' => 'api',
+                        'namespace'  => "Modules\\{$module->get('basename')}\\Http\\Controllers\\API",
+                        'prefix'     => 'api',
+                    ], function ($router) use ($slug) {
+                        require module_path($slug, 'routes/api.php');
+                    });
+                }
+            }
+        }
+    }
+
+    /**
      * Get all of the configuration files for the application.
      *
      * @param  string  $path
